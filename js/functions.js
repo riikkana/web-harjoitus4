66fecc12c013202c83319ca5f3a3e42185e700c9
riikkana <naamanka.riikka@gmail.com>
2024-09-16 09:27:41 +0300

    Final

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+// Hae elementti ID:n perusteella
+const imageElement = document.getElementById("dice");
+
+// Lisää click-tapahtumankuuntelija
+imageElement.addEventListener("click", function() {
+    // Suoritettava toiminto kun kuvaa klikataan
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+    let newImageSrc = "";
+
+    if (randomNumber === 1) {
+        newImageSrc = "./img/1.png";
+    } else if (randomNumber === 2) {
+        newImageSrc = "./img/2.png";
+    } else if (randomNumber === 3) {
+        newImageSrc = "./img/3.png";
+    } else if (randomNumber === 4) {
+        newImageSrc = "./img/4.png";
+    } else if (randomNumber === 5) {
+        newImageSrc = "./img/5.png";
+    } else if (randomNumber === 6) {
+        newImageSrc = "./img/6.png";
+    }
+
+    imageElement.src = newImageSrc;
+});
